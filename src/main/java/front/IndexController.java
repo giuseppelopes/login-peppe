9e9c4eb1be6078e7799ff537aa3a9a2c5d2331b0
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2017-09-26 23:24:16 -0300

    First version

---
+++ src/main/java/front/IndexController.java
@@ -0,0 +1,22 @@
+package front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "";
+    }
+
+}
