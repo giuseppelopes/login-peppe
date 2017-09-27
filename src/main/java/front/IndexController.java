85d4b0037dc741f5de0530029279b73cd4c9b415
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2017-09-26 23:42:44 -0300

    Added form of login

--- src/main/java/front/IndexController.java
+++ src/main/java/front/IndexController.java
@@ -8,15 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
     @RequestMapping("/login")
     public String login(Model model) {
-        return "";
+        return "login";
     }
 
 }
